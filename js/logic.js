2629eb74df0ebfa99eea17b24d228d1e92d5d50e
Brian Jahnke <bjahnke71@gmail.com>
2024-04-09 10:59:17 -0400

    Update display with input from buttons

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,4 @@
-import {add, subtract, multiply, divide} from "./operation";
-
+import { add, subtract, multiply, divide } from './operation'
 
 /**
  * Performs a mathematical operation on two numbers.
@@ -10,16 +9,16 @@ import {add, subtract, multiply, divide} from "./operation";
  * @returns {number} - The result of the operation.
  */
 function operator (first, second, op) {
-    switch(op) {
-        case '+':
-            return add(first, second);
-        case '-':
-            return subtract(first, second);
-        case '*':
-            return multiply(first, second);
-        case '/':
-            return divide(first, second)
-    }
+  switch (op) {
+    case '+':
+      return add(first, second)
+    case '-':
+      return subtract(first, second)
+    case '*':
+      return multiply(first, second)
+    case '/':
+      return divide(first, second)
+  }
 }
 
-export { operator };
+export { operator }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-import { operation } from './logic';
+let first, second, op
 
-let first, second, op;
+const display = document.querySelector('.display')
+
+// allow for 8, 8 *, 8 * 8, but not * 8
+const displayPattern = /^(?:\d+)(?:[+\-*\/]\d*)?$/
+
+/*
+* Translates calculator input to the display
+*/
+function updateDisplay () {
+  const newString = display.textContent + this.textContent
+  if (displayPattern.test(newString)) {
+    display.textContent = newString
+  }
+}
+
+function clearInputs () {
+  display.textContent = ''
+  first = ''
+  second = ''
+  op = ''
+}
+
+const calcButtons = document.querySelectorAll('button.op,button.num')
+console.group(calcButtons)
+calcButtons.forEach(button => button.addEventListener('click', updateDisplay))
